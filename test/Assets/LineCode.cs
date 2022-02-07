eb9896569e024ab3a654b4eab7ddc1ca604f33a8
dtyty1895 <83059760+dtyty1895@users.noreply.github.com>
2022-02-07 10:33:21 +0800

    OpenTxtUnity

---
+++ test/Assets/LineCode.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineCode : MonoBehaviour
+{
+
+    LineRenderer lineRenderer;
+
+    Transform origin;
+    Transform destination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //lineRenderer = 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
